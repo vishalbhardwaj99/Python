554d6e64f424beb628ad9be2891fd28c2fc3d1a5
Vishal Bhardwaj <vishalbhardwaj0571@gmail.com>
2025-04-28 19:41:25 +0530

    ....

---
+++ direli.py
@@ -0,0 +1,5 @@
+import os
+directory_content = '/Users'
+content = os.listdir(directory_content)
+for item in content:
+    print(item)
---
+++ voice.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("You are the goat of goat, 4UD voult")
+engine.runAndWait()
